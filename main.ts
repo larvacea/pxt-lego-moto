e5952ad69131aa4d6dd83acc5f1fb42bcf329ab5
larvacea <larvacea@mac.com>
2019-08-09 09:36:16 -0700

    actually do a thing

--- main.ts
+++ main.ts
@@ -1,7 +1,28 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+twist.setColor(63, 255, 0, 0)
+motobit.enable(MotorPower.On)
+motobit.setMotorSpeed(Motor.Left, MotorDirection.Forward, 0)
+motobit.setMotorSpeed(Motor.Right, MotorDirection.Forward, 0)
+let was = 0
+while (true) {
+    let c = twist.getCount(63)
+    basic.showNumber(c)
+    if (c != was) {
+        was = c
+        if (c > 0) {
+            twist.setColor(63, 0, 255, 0)
+        }
+        else if (c < 0) {
+            twist.setColor(63, 0, 0, 255)
+        }
+        else {
+            twist.setColor(63, 255, 0, 0)
+        }
+        if (c >= 0) {
+            motobit.setMotorSpeed(Motor.Right, MotorDirection.Forward, c)
+        }
+        else {
+            motobit.setMotorSpeed(Motor.Right, MotorDirection.Reverse, c)
+        }
+    }
+    basic.pause(100)
+}
